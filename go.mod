be2253f749ca8f9af2cab315753cdb85a7c3dba3
Sirasit0397 <ssbunklnag@gmail.com>
2022-12-09 06:50:17 +0700

    can CRUD

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/aeff60/go-crud
 
 go 1.17
 
-require github.com/go-sql-driver/mysql v1.7.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,115 @@
+// CRUD API with mysql database
+
 package main
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1.3306)/dbname") // Open doesn't open a connection. Validate DSN data:
-	if err != nil {                                                          // if there is an error opening the connection, handle it here
-		fmt.PrintLn("failed to connect") // proper error handling instead of panic in your app
-	} else { // defer the close till after the main function has finished
-		// executing
-		fmt.PrintLn("connected")
+// Book struct (Model)
+type Book struct {
+	ID     int     `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Init books var as a slice Book struct
+var books []Book
+
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+	// Loop through books and find with id
+	fmt.Println(params["id"])
+	for _, item := range books {
+		if item.ID, _ = strconv.Atoi(params["id"]); item.ID == item.ID {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
 	}
-	fmt.PrintLn(db)
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+// Add new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json") // ตั้งค่า header ให้เป็น json
+
+	var book Book // สร้างตัวแปร book ขึ้นมาเพื่อเก็บค่าที่ส่งมาจาก client
+
+	_ = json.NewDecoder(r.Body).Decode(&book) // แปลงค่าที่ส่งมาจาก client ให้เป็น json แล้วเก็บไว้ในตัวแปร book
+
+	books = append(books, book) // เพิ่มค่าที่ได้จาก client ไปเก็บไว้ในตัวแปร books ที่เป็น slice ของ Book struct ที่เราสร้างไว้ แล้วส่งค่ากลับไปให้ client ที่เรียกใช้ api นี้
+
+	json.NewEncoder(w).Encode(book) // ส่งค่ากลับไปให้ client ที่เรียกใช้ api นี้ โดยแปลงค่าที่เราส่งกลับไปให้เป็น json ก่อน แล้วส่งไป
+}
+
+// Update book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID, _ = strconv.Atoi(params["id"]); item.ID == item.ID {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = item.ID
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+// Delete book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID, _ = strconv.Atoi(params["id"]); item.ID == item.ID {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func main() {
+	// Init Router
+	r := mux.NewRouter()
+
+	// Mock Data - @todo - implement DB
+	books = append(books, Book{ID: 1, Isbn: "448743", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: 2, Isbn: "448744", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
